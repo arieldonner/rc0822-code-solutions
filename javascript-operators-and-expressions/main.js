774c432c15277df40e490449c796ea3248fef5af
arieldonner <arieldonner@gmail.com>
2022-08-29 17:48:39 +0000

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 2;
+var height = 5;
+var area = width * height;
+console.log('Value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 200;
+var payment = 500;
+var change = payment - bill;
+console.log('Value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 97;
+var midterm = 85;
+var final = 94;
+var grade = (quizzes + midterm + final) / 3;
+console.log('Value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Ariel';
+var lastName = 'Donner';
+var fullName = firstName + ' ' + lastName;
+console.log('Value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 6;
+var isAcidic = pH < 7;
+console.log('Value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 100;
+var isSparta = headCount === 300;
+console.log('Value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('Value of motto:', motto);
+console.log('typeof motto:', typeof motto);
