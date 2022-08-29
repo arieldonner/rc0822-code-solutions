72fd325c3d7d80207009751af7b25b703d3b7ace
arieldonner <arieldonner@gmail.com>
2022-08-29 21:53:19 +0000

    Completed javascript-arrays exercise

---
+++ javascript-arrays/main.js
@@ -0,0 +1,16 @@
+var colors = ['red', 'white', 'blue'];
+console.log('Value of colors[0]:', colors[0]);
+console.log('Value of colors[1]:', colors[1]);
+console.log('Value of colors[2]:', colors[2]);
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.');
+colors[2] = 'green';
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.');
+console.log('Value of colors:', colors);
+
+var students = ['Leonardo', 'Donatello', 'Raphael', 'Michelangelo'];
+var numberOfStudents = students.length;
+console.log('Value of numberOfStudents:', numberOfStudents);
+var lastIndex = numberOfStudents - 1;
+var lastStudent = students[lastIndex];
+console.log('Value of lastStudent:', lastStudent);
+console.log('Value of students:', students);
