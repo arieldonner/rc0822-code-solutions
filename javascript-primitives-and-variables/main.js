177863c1da3f46adf61895b11a831b69c6460429
arieldonner <arieldonner@gmail.com>
2022-08-29 16:49:36 +0000

    Added undefined variable and logged it and its typeof to the console

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -10,3 +10,6 @@ console.log('Total Pets typeof:', typeof totalPets);
 var unknown = null;
 console.log('Unknown:', unknown);
 console.log('Unknown typeof:', typeof unknown);
+var never;
+console.log('Never:', never);
+console.log('Never typeof:', typeof never);
