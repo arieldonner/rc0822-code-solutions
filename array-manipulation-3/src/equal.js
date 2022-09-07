fc2d9aebd59550c45908596f55299f2344da668a
arieldonner <arieldonner@gmail.com>
2022-09-07 18:01:16 +0000

    Completed equals and flatten functions

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,10 +1,21 @@
 /* exported equal */
 function equal(first, second) {
-  var stringFirst = first.join();
-  var stringSecond = second.join();
-  if (stringFirst === stringSecond) {
-    return true;
+  if (typeof first[0] !== 'object') {
+    var stringFirst = first.join();
+    var stringSecond = second.join();
+    if (stringFirst === stringSecond) {
+      return true;
+    } else {
+      return false;
+    }
   } else {
-    return false;
+    for (var key in Object.keys(first)) {
+      if (second[key] !== null && first[key] === second[key]) {
+        continue;
+      } else {
+        return false;
+      }
+    }
+    return true;
   }
 }
--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,23 @@
 /* exported flatten */
+function flatten(array) {
+  var newArr = [];
+  for (var i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i]) === true) {
+      var innerArr = array[i];
+      for (var j = 0; j < innerArr.length; j++) {
+        newArr.push(innerArr[j]);
+      }
+    } else {
+      newArr.push(array[i]);
+    }
+  }
+  return newArr;
+}
+
+/* Create a variable to hold the new array
+Loop through each element of the given array.
+Check if the element of the array is an array itself.
+If this is true, loop through each element of that array.
+For each element, push it into the new array.
+If the element is not an array, then just push the element itself into the new array.
+Return the new array from the function. */
