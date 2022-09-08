d295add4fc286a9257fe2591724711e6ca9a92f5
arieldonner <arieldonner@gmail.com>
2022-09-08 21:14:52 +0000

    Completed array-manipulation-3

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,10 @@
 /* exported intersection */
+function intersection(first, second) {
+  var finalArr = [];
+  for (var i = 0; i < first.length; i++) {
+    if (second.includes(first[i])) {
+      finalArr.push(first[i]);
+    }
+  }
+  return finalArr;
+}
