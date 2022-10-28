3b70f2dcff7ec9a525594c9c012b17c20dcdb539
arieldonner <arieldonner@gmail.com>
2022-10-28 23:24:34 +0000

    Changed condition from checking what the event target wasn't to check if the event.target is equal to the event.currentTarget

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -10,23 +10,23 @@ class AppDrawer extends React.Component {
   }
 
   handleClick(event) {
-    if (event.target.className !== 'menu' && event.target.className !== 'rectangle') {
+    if (event.target === event.currentTarget) {
       this.setState({ isOpen: !this.state.isOpen });
     }
   }
 
   render() {
     if (this.state.isOpen === false) {
-      return <FontAwesomeIcon icon={faBars} className='icon' onClick={this.handleClick}/>;
+      return <FontAwesomeIcon icon={faBars} className='icon' id='icon' onClick={this.handleClick}/>;
     }
     if (this.state.isOpen === true) {
       return (
         <div className='modal' onClick={this.handleClick}>
           <div className='rectangle'>
             <h1 className='menu'>Menu</h1>
-            <h3 onClick={this.handleClick}>About</h3>
-            <h3 onClick={this.handleClick}>Get Started</h3>
-            <h3 onClick={this.handleClick}>Sign In</h3>
+            <h3 className='link' onClick={this.handleClick}>About</h3>
+            <h3 className='link' onClick={this.handleClick}>Get Started</h3>
+            <h3 className='link' onClick={this.handleClick}>Sign In</h3>
           </div>
         </div>
       );
