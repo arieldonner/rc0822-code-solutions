f3ccd3fbf99dea5f86381dae78ae125cc61c6673
arieldonner <arieldonner@gmail.com>
2022-10-31 18:10:36 +0000

    Completed react-accordion-component

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
 
-function Accordion(props) {
-  const topics = props.topics;
-  const listItems = topics.map((item, index) =>
-      <li className='list' key={index}>{item.name}</li>
-  );
-  return (
-    <ul>{listItems}</ul>
-  );
+class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentId: 0 };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    if (event.target === event.currentTarget) {
+      if (this.state.currentId === event.target.id) {
+        this.setState({ currentId: 0 });
+      } else {
+        this.setState({ currentId: event.target.id });
+      }
+    }
+  }
+
+  render() {
+    let display = 'hide';
+    const topics = this.props.topics;
+    const listItems = topics.map((item, index) => {
+      if (Number(this.state.currentId) === item.id) {
+        display = 'show';
+      } else {
+        display = 'hide';
+      }
+      return (
+        <div key={index} className='container'>
+          <h3 className='name' id={item.id} onClick={this.handleClick}>{item.name}</h3>
+          <p className={`details ${display}`}>{item.detail}</p>
+        </div>
+      );
+    }
+    );
+    return (
+      <div>{listItems}</div>
+    );
+  }
 }
 
 export default Accordion;
