d793c557ba85478860375789cc51508ad3b5b7f0
arieldonner <arieldonner@gmail.com>
2022-11-01 17:40:32 +0000

    Fixed express.static method to use the variable of path.join

--- express-static/index.js
+++ express-static/index.js
@@ -3,10 +3,9 @@ const app = express();
 
 const path = require('node:path');
 
-const join = path.join(__dirname, 'public');
-console.log(join);
+const publicPath = path.join(__dirname, 'public');
 
-app.use(express.static('public'));
+app.use(express.static(publicPath));
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
